8b89b7e02844434ac5aaeedfb655d71fe4693f42
Qiao Wang <wangqiao11@hotmail.com>
2024-07-26 23:07:11 +0800

    Init MAUI.

---
+++ src/PwSafeClient.Maui/App.xaml.cs
@@ -0,0 +1,12 @@
+namespace PwSafeClient.Maui
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
