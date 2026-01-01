ed3dc78ced9f33307b942ed33cf2adc9fe353d29
Qiao Wang <wangqiao11@hotmail.com>
2026-01-01 23:59:25 +0800

    Fix format

--- src/PwSafeClient.CLI/Infrastructure/TypeRegistrar.cs
+++ src/PwSafeClient.CLI/Infrastructure/TypeRegistrar.cs
@@ -1,5 +1,7 @@
 using System;
+
 using Microsoft.Extensions.DependencyInjection;
+
 using Spectre.Console.Cli;
 
 namespace PwSafeClient.Cli.Infrastructure;
--- test/PwSafeClient.CLI.UnitTests/CliIntegrationTests.cs
+++ test/PwSafeClient.CLI.UnitTests/CliIntegrationTests.cs
@@ -3,8 +3,8 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Text;
-using System.Threading.Tasks;
 using System.Threading;
+using System.Threading.Tasks;
 
 using Medo.Security.Cryptography.PasswordSafe;
 
